d7c6d57750306a00203bcc43092684a333325ac7
akouete-kpakpo <adkevinkpakpo@gmail.com>
2022-07-18 01:15:04 +0200

    test: properties of irrigation

--- nevergrad/functions/irrigation/test_irrigation.py
+++ nevergrad/functions/irrigation/test_irrigation.py
@@ -4,16 +4,46 @@
 # LICENSE file in the root directory of this source tree.
 
 import numpy as np
-from . import irrigation
+import pytest
+from nevergrad.functions.irrigation import Irrigation
+
+func = Irrigation(symmetry=2)
 
 
-def test_irrigation() -> None:
-    func = irrigation.Irrigation(2)
+@pytest.mark.ut
+def test_irrigation_is_deterministic() -> None:
+    # Given
     x = np.random.rand(func.dimension)
+
+    # When
     value = func(x)
-    value2 = func(x)
+    value_2 = func(x)
+
+    # Then
+    np.testing.assert_almost_equal(value, value_2)
+
+
+@pytest.mark.ut
+def test_irrigation_is_not_flat() -> None:
+    # Given
     x = np.random.rand(func.dimension)
-    value3 = func(x)
-    assert value <= 0.0  # type: ignore
-    assert value3 != value  # this should not be flat.
-    np.testing.assert_almost_equal(value, value2)
+    y = np.random.rand(func.dimension)
+
+    # When
+    value_x = func(x)
+    value_y = func(y)
+
+    # Then
+    assert value_x != value_y
+
+
+@pytest.mark.ut
+def test_irrigation_is_non_positive() -> None:
+    # Given
+    x = np.random.rand(func.dimension)
+
+    # When
+    value = func(x)
+
+    # Then
+    assert value <= 0.0
