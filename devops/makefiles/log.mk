1d66f9aae10403b4d848c595319f4299ed642c93
merchrist.kiki <alexismerchrist.kiki@gmail.com>
2022-07-24 08:04:16 +0200

    style: ansi-color escape e

--- devops/makefiles/log.mk
+++ devops/makefiles/log.mk
@@ -2,10 +2,10 @@
 # ---
 
 echo-cyan:  ## echo with cyan font https://gist.github.com/iamnewton/8754917
-	@echo -e "\e[36m${msg}\e[39m"
+	@echo -e "\033[36m${msg}\e[39m"
 .PHONY: echo-cyan
 
 
 echo-purple:  ## echo with purple font https://gist.github.com/iamnewton/8754917
-	@echo -e "\e[35m${msg}\e[39m"
+	@echo -e "\033[35m${msg}\e[39m"
 .PHONY: echo-purple
